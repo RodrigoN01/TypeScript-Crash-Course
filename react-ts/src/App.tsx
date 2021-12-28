e47353c4c9107baaeddd6ebb5ecec5da1858b3f7
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-12-29 12:25:29 +1300

    Basic TS with React

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Hello World' color='red' />
+    </div>
+  );
+}
+
+export default App;
